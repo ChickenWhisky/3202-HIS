59cd65943575331d703784209dafffb6b54bc557
Ayush Chadha <elegantShock3307@gmail.com>
2023-09-03 12:08:29 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  // TODO: figure out how to run a script with args and permissions and show the o/p and progress bar and errors if any
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
